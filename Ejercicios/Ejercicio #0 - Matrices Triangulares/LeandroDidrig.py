169505da0c826d6288511df5ef5514260baaed11
LDidrig <didrigleandro420@gmail.com>
2024-10-04 18:57:41 -0300

    Update LeandroDidrig.py

--- Ejercicios/Ejercicio #0 - Matrices Triangulares/LeandroDidrig.py
+++ Ejercicios/Ejercicio #0 - Matrices Triangulares/LeandroDidrig.py
@@ -1,11 +1,36 @@
+def separar_numeros(numeros,n):
+    e = [int(0)]*100
+    numbers = [int(0)]*n
+    f = 1
+    for i in range(len(numeros)):
+        if (numeros[i]==' '):
+            e[f] = i
+            f += 1
+    e[f] = len(numeros)
+    b = 0
+    a = 1
+    for i in range(f):
+        if i == 0:
+            numbers[i] = int(numeros[e[b]:e[a]])
+            a += 1
+            b += 1
+        else:  
+            numbers[i] = int(numeros[e[b]+1:e[a]])
+            a += 1
+            b += 1
+    return numbers
+
 n = int(input())
+VoF = [str(0)]*1000
+indice = 0
+k = 0
 while n!=0:
     matriz = [[int(0) for i in range(n)]for j in range(n)]
     inferior = 0
     superior = 0
     for i in range(n):
         num = str(input())
-        filita = num.split()
+        filita = separar_numeros(num,n)
         for j in range(n):
             filita[j] = int(filita[j])
         matriz[i] = filita
@@ -20,7 +45,12 @@ while n!=0:
                 if matriz[i][j] != 0:
                     superior = 1
     if inferior == 1 and superior == 1:
-        print('NO')
+        VoF[indice] = "NO"
+        indice += 1
     else:
-        print('SI')
+        VoF[indice] = "SI"
+        indice += 1
+    k += 1
     n = int(input())
+for i in range(k):
+    print(VoF[i])
